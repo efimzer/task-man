8a2278bc69a15c18b0e6149eb6f60a22908a31f5
efimzer <efimzer@gmail.com>
2025-09-27 00:44:27 +0300

    update url

--- scripts/sync-config.js
+++ scripts/sync-config.js
@@ -1,6 +1,6 @@
 export const syncConfig = {
   enabled: true,
-  baseUrl: 'http://192.168.1.90:8787',
+  baseUrl: 'https://task-man-rf22.onrender.com',
   userId: 'efimzer',
   authToken: '7a48ead5282819ee63d3b3f78eaf8a130ecde091f2418a747f8270a9ff6d816e',
   pullOnStartup: true,
