cecc3d41a9bebfb0b9810924a2762a1d0e7d9c46
efimzer <efimzer@gmail.com>
2025-09-28 02:13:48 +0300

    update auth4

--- scripts/sync-config.js
+++ scripts/sync-config.js
@@ -3,9 +3,9 @@ export const syncConfig = {
   baseUrl: 'https://task-man-rf22.onrender.com',
   userId: 'efimzer@gmail.com',
   authToken: '7a48ead5282819ee63d3b3f78eaf8a130ecde091f2418a747f8270a9ff6d816e',
-  authTokenInQuery: true,
-  useSessionAuth: false,
+  authTokenInQuery: false,
+  useSessionAuth: true,
   pullOnStartup: true,
-  pushDebounceMs: 100,
+  pushDebounceMs: 500,
   pullIntervalMs: 500
 };
