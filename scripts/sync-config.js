e5658ea07c059d12fbdb9cce551d5dbd3fc1e342
efimzer <efimzer@gmail.com>
2025-09-28 02:24:33 +0300

    update auth5

--- scripts/sync-config.js
+++ scripts/sync-config.js
@@ -1,8 +1,8 @@
 export const syncConfig = {
   enabled: true,
   baseUrl: 'https://task-man-rf22.onrender.com',
-  userId: 'efimzer@gmail.com',
-  authToken: '7a48ead5282819ee63d3b3f78eaf8a130ecde091f2418a747f8270a9ff6d816e',
+  userId: '',
+  authToken: '',
   authTokenInQuery: false,
   useSessionAuth: true,
   pullOnStartup: true,
