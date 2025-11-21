35503c9d58cbda409ddd2b124d52e153e851a86d
efimzer <efimzer@gmail.com>
2025-11-21 19:17:19 +0300

    web

--- scripts/sync-config.js
+++ scripts/sync-config.js
@@ -1,6 +1,6 @@
 export const syncConfig = {
   enabled: true,
-  baseUrl: 'https://gofima.online/web',
+  baseUrl: 'https://gofima.online',
   pullOnStartup: true,
   pushDebounceMs: 500,
   pullIntervalMs: 1500,
